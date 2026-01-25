fd4267239128dd4e9ebc9a273ad8f9b0aed30de7
dev.ventogroup@gmail.com <dev.ventogroup@gmail.com>
2026-01-25 18:02:36 -0500

    4

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,7 @@
-export { middleware, config } from "@devventogroup/auth-middleware";
+import { middleware as authMiddleware } from "@devventogroup/auth-middleware";
+
+export const config = {
+  matcher: ["/((?!_next|login|favicon.ico|logos|images|fonts|api).*)"],
+};
+
+export const middleware = authMiddleware;
